ca1c5611f6c1a2a65ef817158de0664ee0912a78
vrusi <v.rusinkova97@gmail.com>
2021-03-21 16:47:44 +0100

    wip

--- hf-app/resources/views/dashboard.blade.php
+++ hf-app/resources/views/dashboard.blade.php
@@ -4,8 +4,26 @@
 @section('content')
 
 <div class="container">
-    Dashboard
+    <div id="myDiv"></div>
 </div>
 
-@endsection
+<script>
+    var trace1 = {
+        x: [1, 2, 3, 4]
+        , y: [10, 15, 13, 17]
+        , type: 'scatter'
+    };
 
+    var trace2 = {
+        x: [1, 2, 3, 4]
+        , y: [16, 5, 11, 9]
+        , type: 'scatter'
+    };
+
+    var data = [trace1, trace2];
+
+    Plotly.newPlot('myDiv', data);
+
+</script>
+
+@endsection
--- hf-app/resources/views/layouts/app.blade.php
+++ hf-app/resources/views/layouts/app.blade.php
@@ -21,8 +21,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <!-- Plotly CDN -->
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 </head>
 <body>
     @include('partials.navigation')
