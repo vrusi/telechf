63a7a75c41b61088f5c2eb2262a356da82097711
vrusi <v.rusinkova97@gmail.com>
2021-03-21 22:23:35 +0100

    Display + pagination of measurements

--- hf-app/app/Http/Controllers/Patient/MeasurementController.php
+++ hf-app/app/Http/Controllers/Patient/MeasurementController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Patient;
 
 use App\Http\Controllers\Controller;
 use App\Models\Measurement;
+use App\Models\Parameter;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Minwork\Helper\Arr;
 
 class MeasurementController extends Controller
 {
@@ -14,11 +17,54 @@ class MeasurementController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $measurements = Auth::user()->measurements()->get();
-        dd($measurements);
-        return view('patient.measurements.index', ['measurements' => $measurements]);
+        $date = Carbon::today();
+
+        if ($request->has('date')) {
+            $date = Carbon::createFromFormat('Y-m-d', $request->input('date'));
+        }
+
+        $parameters = Parameter::all()->toArray();
+
+        $measurements = Measurement::where('user_id', Auth::user()->id)->whereDate('created_at', $date)->get()->toArray();
+
+        $values = array_map(function ($parameter) use ($measurements) {
+            $value = null;
+
+            foreach ($measurements as $measurement) {
+                if ($measurement['parameter_id'] == $parameter['id']) {
+                    $value = $measurement['value'];
+                }
+            }
+
+            return ['parameter' => $parameter['name'], 'value' => $value, 'unit' => $parameter['unit']];
+        }, $parameters);
+
+        $conditions = Arr::map(['swellings' => 'Swellings', 'exercise_tolerance' => 'Exercise Tolerance', 'dyspnoea' => 'Nocturnal Dyspnoea'], function ($key, $name) use ($measurements) {
+            $avg = null;
+
+            if (count($measurements) > 0) {
+                $avg = 0;
+                foreach ($measurements as $measurement) {
+                    $avg = $avg + $measurement[$key] ?? 0;
+                }
+                $avg = $avg / count($measurements);
+            }
+
+            return ['name' => $name, 'value' => $avg];
+        });
+
+        if ($date->isToday()) {
+            $next = null;
+        } else {
+            $next = 'measurements?date=' . $date->copy()->addDays(1)->format('Y-m-d');
+        }
+
+        $prev = 'measurements?date=' . $date->copy()->subDays(1)->format('Y-m-d');
+
+        $results = ['measurements' => $values, 'conditions' => array_values($conditions), 'date' => $date, 'previous' => $prev, 'next' => $next];
+        return view('patient.measurements.index', $results);
     }
 
     /**
