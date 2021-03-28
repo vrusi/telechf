14b05dbb9dd266b77eac65c594197d74a712e1d1
vrusi <v.rusinkova97@gmail.com>
2021-03-28 16:35:14 +0200

    Add conditions and drugs to patient treatment

--- chf/resources/views/patient/therapy/index.blade.php
+++ chf/resources/views/patient/therapy/index.blade.php
@@ -63,5 +63,60 @@
             @endforeach
         </tbody>
     </table>
+
+
+    <h2>
+        Recommendations
+    </h2>
+    <div>
+        TODO
+    </div>
+
+    <h2>
+        You are being treated for
+    </h2>
+    @foreach($conditions as $condition)
+    <div>
+        {{ $condition->name }}
+    </div>
+    @endforeach
+
+    <h2>
+        You are currently prescribed
+    </h2>
+    <table>
+        @foreach($drugs as $drug)
+        <tr>
+            <td class="font-weight-bold pr-3">
+                {{ $drug->name }}
+
+            </td>
+
+            <td class="pr-3">
+                {{ $drug->dosage_volume.' '.$drug->dosage_unit }}
+            </td>
+
+            <td class="pr-3">
+                @if($drug->dosage_times)
+                @if($drug->dosage_times == 1)
+                {{ 'once per '.$drug->dosage_span }}
+                @endif
+                @if($drug->dosage_times == 2)
+                {{ 'twice per '.$drug->dosage_span }}
+                @endif
+                @if($drug->dosage_times >= 3)
+                {{ $drug->dosage_times.' times per '.$drug->dosage_span }}
+                @endif
+                @endif
+
+                @if(!$drug->dosage_times)
+                {{ '--' }}
+                @endif
+            </td>
+        </tr>
+
+        @endforeach
+    </table>
+
 </div>
 @endsection
