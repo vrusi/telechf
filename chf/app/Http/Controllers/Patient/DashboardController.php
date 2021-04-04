cb76b4bcd076cc5f0495cfff8869252d0e9c61d5
vrusi <me@vrusinkova.com>
2021-04-05 01:25:59 +0200

    Move user measurements logic to the User model

--- chf/app/Http/Controllers/Patient/DashboardController.php
+++ chf/app/Http/Controllers/Patient/DashboardController.php
@@ -12,97 +12,12 @@ use Minwork\Helper\Arr;
 
 class DashboardController extends Controller
 {
-    private function mapConditions(int $value)
-    {
-        if ($value == 5) {
-            return 'Very bad';
-        } else if ($value == 4) {
-            return 'Bad';
-        } else if ($value == 3) {
-            return 'Neutral';
-        } else if ($value == 2) {
-            return 'Good';
-        } else if ($value == 1) {
-            return 'Very good';
-        }
-
-        return null;
-    }
-
-
     function index(Request $request)
     {
         $user = Auth::user();
         $parameters = $user->parameters->toArray();
-
-        $measurementsGrouped = Measurement::where('user_id', $user->id)->orderBy('created_at', 'desc')->get()->groupBy(function ($measurement) {
-            return $measurement->created_at->format('d M');
-        })->toArray();
-
-        // TODO
-        $alarmGeneral = false;
-
-        $daysSummary = array_map(function ($measurementsPerDay) use ($parameters) {
-            $values = array_map(function ($parameter) use ($measurementsPerDay) {
-                $value = null;
-                $alarm = false;
-
-                foreach ($measurementsPerDay as $measurement) {
-                    if ($measurement['parameter_id'] == $parameter['id']) {
-                        $value = $measurement['value'];
-
-                        $alarm = $measurement['triggered_therapeutic_alarm_min'] || $measurement['triggered_therapeutic_alarm_max'] ||  $measurement['triggered_safety_alarm_min'] || $measurement['triggered_safety_alarm_max'];
-
-                        if ($alarm) {
-                            $alarmGeneral = true;
-                        }
-                    }
-                }
-
-                return ['parameter' => $parameter['name'], 'value' => $value, 'unit' => $parameter['unit'], 'alarm' => $alarm, 'date' => $measurement['created_at']];
-            }, $parameters);
-
-            $conditions = Arr::map(['swellings' => 'Swellings', 'exercise_tolerance' => 'Exercise Tolerance', 'dyspnoea' => 'Nocturnal Dyspnoea'], function ($key, $name) use ($measurementsPerDay) {
-                $avg = null;
-                $avgMapped = '';
-                // TODO
-                $alarm = false;
-
-                if (count($measurementsPerDay) > 0) {
-                    $avg = 0;
-                    foreach ($measurementsPerDay as $measurement) {
-                        $avg = $avg + $measurement[$key] ?? 0;
-                    }
-                    $avg = $avg / count($measurementsPerDay);
-
-                    $avgMapped = $this->mapConditions(ceil($avg));
-
-                }
-
-                return ['name' => $name, 'value' => $avgMapped, 'alarm' => $alarm];
-            });
-
-            $conditions = array_values($conditions);
-
-            return array_merge($values, $conditions);
-        }, $measurementsGrouped);
-
-        $daysAlarms = array();
-        foreach($daysSummary as $date => $day) {
-            foreach ($day as $measurement) {
-
-                if (!$measurement['alarm']) {
-                    continue;
-                }
-
-                if (in_array($day, $daysAlarms)) {
-                    continue;
-                }
-
-                $daysAlarms[$date] = $day;
-            }
-        }
-
-        return view('patient.dashboard.index', ['summary' => $daysSummary, 'alarms' => $daysAlarms, 'parameters' => $parameters]);
+        $summary = $user->measurementsSummary();
+        $alarms = $user->measurementsAlarms();
+        return view('patient.dashboard.index', ['summary' => $summary, 'alarms' => $alarms, 'parameters' => $parameters]);
     }
 }
--- chf/app/Models/User.php
+++ chf/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Minwork\Helper\Arr;
 
 class User extends Authenticatable
 {
@@ -30,6 +31,23 @@ class User extends Authenticatable
         'coordinator_id',
     ];
 
+    private function mapConditions(int $value)
+    {
+        if ($value == 5) {
+            return 'Very bad';
+        } else if ($value == 4) {
+            return 'Bad';
+        } else if ($value == 3) {
+            return 'Neutral';
+        } else if ($value == 2) {
+            return 'Good';
+        } else if ($value == 1) {
+            return 'Very good';
+        }
+
+        return null;
+    }
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -52,6 +70,85 @@ class User extends Authenticatable
     public function measurements()
     {
         return $this->hasMany(Measurement::class);
+    }
+
+    public function measurementsByDay()
+    {
+        return Measurement::where('user_id', $this->id)->orderBy('created_at', 'desc')->get()->groupBy(function ($measurement) {
+            return $measurement->created_at->format('d M');
+        })->toArray();
+    }
+
+    public function measurementsSummary()
+    {
+        $parameters = $this->parameters->toArray();
+        $measurementsGrouped = $this->measurementsByDay();
+
+        return array_map(function ($measurementsPerDay) use ($parameters) {
+            $values = array_map(function ($parameter) use ($measurementsPerDay) {
+                $value = null;
+                $alarm = false;
+
+                foreach ($measurementsPerDay as $measurement) {
+                    if ($measurement['parameter_id'] == $parameter['id']) {
+                        $value = $measurement['value'];
+
+                        $alarm = $measurement['triggered_therapeutic_alarm_min'] || $measurement['triggered_therapeutic_alarm_max'] ||  $measurement['triggered_safety_alarm_min'] || $measurement['triggered_safety_alarm_max'];
+
+                        if ($alarm) {
+                            $alarmGeneral = true;
+                        }
+                    }
+                }
+
+                return ['parameter' => $parameter['name'], 'value' => $value, 'unit' => $parameter['unit'], 'alarm' => $alarm, 'date' => $measurement['created_at']];
+            }, $parameters);
+
+            $conditions = Arr::map(['swellings' => 'Swellings', 'exercise_tolerance' => 'Exercise Tolerance', 'dyspnoea' => 'Nocturnal Dyspnoea'], function ($key, $name) use ($measurementsPerDay) {
+                $avg = null;
+                $avgMapped = '';
+                // TODO
+                $alarm = false;
+
+                if (count($measurementsPerDay) > 0) {
+                    $avg = 0;
+                    foreach ($measurementsPerDay as $measurement) {
+                        $avg = $avg + $measurement[$key] ?? 0;
+                    }
+                    $avg = $avg / count($measurementsPerDay);
+
+                    $avgMapped = $this->mapConditions(ceil($avg));
+                }
+
+                return ['name' => $name, 'value' => $avgMapped, 'alarm' => $alarm];
+            });
+
+            $conditions = array_values($conditions);
+
+            return array_merge($values, $conditions);
+        }, $measurementsGrouped);
+    }
+
+    public function measurementsAlarms()
+    {
+        $alarms = array();
+        $summary = $this->measurementsSummary();
+        foreach ($summary as $date => $day) {
+            foreach ($day as $measurement) {
+
+                if (!$measurement['alarm']) {
+                    continue;
+                }
+
+                if (in_array($day, $alarms)) {
+                    continue;
+                }
+
+                $alarms[$date] = $day;
+            }
+        }
+
+        return $alarms;
     }
 
     public function parameters()
@@ -78,13 +175,13 @@ class User extends Authenticatable
     {
         $parametersGlobal =  Parameter::all();
         $parametersPersonal =  $this->parameters;
-        
+
         $thresholds = array();
-        
+
         foreach ($parametersPersonal as $personal) {
             $safetyMax = $personal->pivot->threshold_safety_max ?? null;
             $safetyMin = $personal->pivot->threshold_safety_min ?? null;
-            
+
             $therapeuticMax = $personal->pivot->threshold_therapeutic_max ?? null;
             $therapeuticMin = $personal->pivot->threshold_therapeutic_min ?? null;
 
@@ -101,9 +198,15 @@ class User extends Authenticatable
                 'safetyMax' => $safetyMax,
                 'safetyMin' => $safetyMin,
                 'therapeuticMax' => $therapeuticMax,
-                'therapeuticMin' => $therapeuticMin];
+                'therapeuticMin' => $therapeuticMin
+            ];
         }
-        
+
         return $thresholds;
+    }
+
+    public function patients()
+    {
+        return $this->hasMany(User::class, 'coordinator_id');
     }
 }
