dd6390b8ffc8ff9ed0d4d733f9598f13e707be95
vrusi <me@vrusinkova.com>
2021-05-10 01:01:10 +0200

    Remove measurement form for ecg

--- chf/resources/views/patient/measurements/create.blade.php
+++ chf/resources/views/patient/measurements/create.blade.php
@@ -21,7 +21,7 @@
                 </h2>
                 @if (!empty($takeToday))
                     @foreach ($takeToday as $parameter)
-                        @if ($parameter->fillable && strtolower($parameter->name) != 'ecg')
+                        @if ($parameter->fillable)
                             <div class="my-3">
                                 <a class="btn btn-outline-primary w-100 text-left"
                                     href="{{ url('/measurements/create/' . $parameter->id) }}">
@@ -46,7 +46,7 @@
 
                 @if (!empty($takeThisWeek))
                     @foreach ($takeThisWeek as $parameter)
-                        @if ($parameter->fillable && strtolower($parameter->name) != 'ecg')
+                        @if ($parameter->fillable)
                             <div class="my-3">
                                 <a class="btn btn-outline-primary w-100 text-left"
                                     href="{{ url('/measurements/create/' . $parameter->id) }}">
@@ -77,14 +77,14 @@
 
                 @if (!empty($extra))
                     @foreach ($extra as $parameter)
-                        @if($parameter->fillable && strtolower($parameter->name) != 'ecg')
-                            
-                        <div class="my-3">
-                            <a class="btn btn-outline-primary w-100 text-left"
-                                href="{{ url('/measurements/create/' . $parameter['id'] . '?extra=1') }}">
-                                {{ __($parameter['name']) }}
-                            </a>
-                        </div>
+                        @if ($parameter->fillable)
+
+                            <div class="my-3">
+                                <a class="btn btn-outline-primary w-100 text-left"
+                                    href="{{ url('/measurements/create/' . $parameter['id'] . '?extra=1') }}">
+                                    {{ __($parameter['name']) }}
+                                </a>
+                            </div>
                         @endif
                     @endforeach
                 @else
--- chf/resources/views/patient/measurements/form.blade.php
+++ chf/resources/views/patient/measurements/form.blade.php
@@ -2,125 +2,141 @@
 
 @section('content')
 
-<style>
-    .instructions {
-        max-width: 300px;
-        position: absolute;
-        left: 5vw;
-        padding: 0 1rem;
-    }
-
-    @media(min-width:1400px) {
+    <style>
         .instructions {
-            max-width: 400px;
+            max-width: 300px;
+            position: absolute;
+            left: 5vw;
+            padding: 0 1rem;
         }
-    }
 
-    @media(min-width:1650px) {
-        .instructions {
-            max-width: 500px;
+        @media(min-width:1400px) {
+            .instructions {
+                max-width: 400px;
+            }
         }
-    }
 
-    @media(min-width:2000px) {
-        .instructions {
-            left: 10vw;
+        @media(min-width:1650px) {
+            .instructions {
+                max-width: 500px;
+            }
         }
-    }
 
-    @media(min-width:2300px) {
-        .instructions {
-            left: 15vw;
+        @media(min-width:2000px) {
+            .instructions {
+                left: 10vw;
+            }
         }
-    }
 
-    @media(min-width:2300px) {
-        .instructions {
-            left: 15vw;
+        @media(min-width:2300px) {
+            .instructions {
+                left: 15vw;
+            }
         }
-    }
 
-</style>
+        @media(min-width:2300px) {
+            .instructions {
+                left: 15vw;
+            }
+        }
 
-<div class="d-none d-xl-block instructions">
-    <h2 class="pb-3">
-        {{ __('Instructions') }}
-    </h2>
-    <div class="text-justify">
-        @if ($locale == 'en')
-        {!! $parameter->instructions_en !!}
-        @elseif ($locale == 'sk')
-        {!! $parameter->instructions_sk !!}
-        @endif
+    </style>
+
+    @if(strtolower($parameter->name) != 'ecg')
+    <div class="d-none d-xl-block instructions">
+        <h2 class="pb-3">
+            {{ __('Instructions') }}
+        </h2>
+        <div class="text-justify">
+            @if ($locale == 'en')
+                {!! $parameter->instructions_en !!}
+            @elseif ($locale == 'sk')
+                {!! $parameter->instructions_sk !!}
+            @endif
+        </div>
     </div>
-</div>
-
-<div class="container patient">
-    <h1 class="pb-3">
-        {{ __('Measurement form') }}
-    </h1>
-
-    @if($extra)
-    <h2>
-        {{ __($parameter->name) }} - {{ __('extra measurement') }}
-    </h2>
-    @else
-    <h2>
-        {{ __($parameter->name) }}
-    </h2>
     @endif
 
-    <div class="d-xl-none" x-data="{ instructionsOpen: false }">
-        <div @click="instructionsOpen=!instructionsOpen" class="d-flex" data-toggle="collapse" data-target="#instructions" aria-expanded="false" aria-controls="instructions">
-            <button class="btn btn-outline-secondary">
-                <div class="d-flex align-items-center">
-                    <div class="mr-3">
-                        {{ __('Tap to read instructions') }}
-                    </div>
+    <div class="container patient">
+        <h1 class="pb-3">
+            {{ __('Measurement form') }}
+        </h1>
 
-                    <i x-show="!instructionsOpen" class="fas fa-caret-down"></i>
-                    <i x-show="instructionsOpen" class="fas fa-caret-up"></i>
-                </div>
-            </button>
-
-        </div>
-
-        <div class="collapse" id="instructions">
-            <div class="card card-body">
-                @if ($locale == 'en')
-                {!! $parameter->instructions_en !!}
-                @elseif ($locale == 'sk')
-                {!! $parameter->instructions_esk !!}
-                @endif
-            </div>
-        </div>
-    </div>
-
-    @php
-    $extra = Request::query('extra');
-    @endphp
-
-    @if($extra)
-    <form method="POST" action="/measurements?extra=1" class="m-5">
+        @if ($extra)
+            <h2>
+                {{ __($parameter->name) }} - {{ __('extra measurement') }}
+            </h2>
         @else
-        <form method="POST" action="/measurements" class="m-5">
+            <h2>
+                {{ __($parameter->name) }}
+            </h2>
+        @endif
+
+        @if(strtolower($parameter->name) == 'ecg')
+            {{ __('Please use the mobile application to upload your ECG data.') }}
+
+            <div class="d-flex justify-content-center">
+                <a class="btn btn-secondary w-50 my-5" href="{{ route('measurements.create') }}">
+                    {{ __('Back') }}
+                </a>
+            </div>
+        @else
+
+            <div class="d-flex justify-content-center d-xl-none" x-data="{ instructionsOpen: false }">
+                <div @click="instructionsOpen=!instructionsOpen" class="d-flex" data-toggle="collapse"
+                    data-target="#instructions" aria-expanded="false" aria-controls="instructions">
+                    <button class="btn btn-outline-secondary">
+                        <div class="d-flex align-items-center justify-content-center">
+                            <div class="mr-3 ">
+                                {{ __('Tap to read instructions') }}
+                            </div>
+
+                            <i x-show="!instructionsOpen" class="fas fa-caret-down"></i>
+                            <i x-show="instructionsOpen" class="fas fa-caret-up"></i>
+                        </div>
+                    </button>
+
+                </div>
+
+                <div class="collapse" id="instructions">
+                    <div class="card card-body">
+                        @if ($locale == 'en')
+                            {!! $parameter->instructions_en !!}
+                        @elseif ($locale == 'sk')
+                            {!! $parameter->instructions_esk !!}
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            @php
+                $extra = Request::query('extra');
+            @endphp
+
+            @if ($extra)
+                <form method="POST" action="/measurements?extra=1" class="m-5">
+                @else
+                    <form method="POST" action="/measurements" class="m-5">
             @endif
 
             @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li class="text-red-500">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
             @csrf
+
+
             <div class="mb-5">
                 <input type="hidden" name="parameter_id" value="{{ $parameter->id }}">
                 <div class="form-group">
                     <label for="value">{{ __($parameter->name) }} ({{ __($parameter->unit) }})</label>
-                    <input required type="number" step="0.01" class="form-control" name="value" id="value" placeholder="{{ __('Enter your measurement') }}">
+                    <input required type="number" step="0.01" class="form-control" name="value" id="value"
+                        placeholder="{{ __('Enter your measurement') }}">
                 </div>
 
                 <div class="form-group">
@@ -163,11 +179,12 @@
                     </select>
                 </div>
 
-                @if($extra)
-                <div class="form-group">
-                    <label for="extraDescription">{{ __('Why did you take the extra measurement?') }}</label>
-                    <textarea class="form-control" id="extraDescription" name="extraDescription" rows="3" placeholder="{{ __("Describe how you're feeling") }}"></textarea>
-                </div>
+                @if ($extra)
+                    <div class="form-group">
+                        <label for="extraDescription">{{ __('Why did you take the extra measurement?') }}</label>
+                        <textarea class="form-control" id="extraDescription" name="extraDescription" rows="3"
+                            placeholder="{{ __("Describe how you're feeling") }}"></textarea>
+                    </div>
                 @endif
             </div>
 
@@ -192,6 +209,8 @@
                     </div>
                 </div>
             </div>
+        @endif
+
         </form>
-</div>
+    </div>
 @endsection
